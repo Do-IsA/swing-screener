f2752c94e937c21d94ed03face2d899c2a5f3a75
is__A <86733398+Do-IsA@users.noreply.github.com>
2026-06-05 09:20:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def make_result(
 
 def analyze_stock(code, name, marcap):
     if code == "005930":
-    st.write(df.tail(10))
+        st.write(df.tail(10))
     start = (get_kst_now() - timedelta(days=160)).strftime("%Y-%m-%d")
     df = load_ohlcv(code, start)
     if df is None or len(df) < 80:
